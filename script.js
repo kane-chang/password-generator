dc43102c033af56cc95f4f19f0402835ac4733f4
Kane Chang <kane.chang.fl@gmail.com>
2023-10-25 20:02:01 +0100

    added getRandom function and generatePassword function

--- script.js
+++ script.js
@@ -88,7 +88,7 @@ var upperCasedCharacters = [
   'Z'
 ];
 
-// Password criteria variables
+// Password criteria variables:
 var passwordLength;
 var lowerCase;
 var upperCase;
@@ -111,7 +111,8 @@ function getPasswordOptions() {
   alert(`The length of your password will be: ${passwordLength}`); //check selected length
 
   // Character type selection (with validation):
-  while (!lowerCase && !upperCase && !numeric && !special) {
+  let selectedCharTypes = false
+  while (!selectedCharTypes) {
     alert("Next, select the character types for your password.\n(At least one character type has to be selected)");
     lowerCase = confirm("Lowercase characters?");
     upperCase = confirm("Uppercase characters?");
@@ -120,9 +121,11 @@ function getPasswordOptions() {
 
     if (!lowerCase && !upperCase && !numeric && !special) {
       alert("You did not select any character types, try again.")
-    };
+    } else {
+      break
+    }
   };
-  
+
   alert(`You have selected the following character types:
   Lowercase Characters: ${lowerCase}
   Uppercase Characters: ${upperCase}
@@ -134,17 +137,60 @@ function getPasswordOptions() {
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-
-}
+  let random =
+    Math.floor(Math.random() * arr.length);
+  // console.log("Random Number Generated : " + random); <--- test logs
+  // console.log(arr[random])
+  character = arr[random]
+  return character
+};
 
 // Function to generate password with user input
 function generatePassword() {
   getPasswordOptions();
-  console.log(passwordLength);
-  console.log(lowerCase);
-  console.log(upperCase);
-  console.log(numeric);
-  console.log(special);
+  // console.log(passwordLength); <-- test logs
+  // console.log(lowerCase);
+  // console.log(upperCase);
+  // console.log(numeric);
+  // console.log(special);
+
+  // Password generation variables:
+  var passwordGenerated = "";
+  var passwordGeneratedLength = 0;
+  var selectedCharactersTypes = []
+
+  // Conditionals checking for selected character types to be included in generated password:
+  if (lowerCase) {
+    passwordGenerated += getRandom(lowerCasedCharacters);
+    passwordGeneratedLength += 1;
+    selectedCharactersTypes = selectedCharactersTypes.concat(lowerCasedCharacters)
+  };
+  if (upperCase) {
+    passwordGenerated += getRandom(upperCasedCharacters);
+    passwordGeneratedLength += 1;
+    selectedCharactersTypes = selectedCharactersTypes.concat(upperCasedCharacters)
+  };
+  if (numeric) {
+    passwordGenerated += getRandom(numericCharacters);
+    passwordGeneratedLength += 1;
+    selectedCharactersTypes = selectedCharactersTypes.concat(numericCharacters)
+  };
+  if (special) {
+    passwordGenerated += getRandom(specialCharacters);
+    passwordGeneratedLength += 1;
+    selectedCharactersTypes = selectedCharactersTypes.concat(specialCharacters)
+  };
+  // console.log(passwordGenerated);
+  // console.log(passwordGeneratedLength);
+  // console.log(selectedCharactersTypes);
+
+  // For loop to randomly select remaining password characters from selected character types
+  for (let i = 0; i < (passwordLength - passwordGeneratedLength); i++) {
+    const newChar = getRandom(selectedCharactersTypes);
+    passwordGenerated += newChar
+  };
+  // console.log(passwordGenerated);
+  return passwordGenerated;
 }
 
 // Get references to the #generate element
